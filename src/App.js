56b25922e251051b9cc523c6ed28709e6e5b8300
Danielle Souza <danielle.andrade@meta.com.br>
2021-11-22 19:18:20 -0300

    feat: criando hook customizado

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import './App.css';
 import FormularioCadastro from './Components/FormularioCadastro/FormularioCadastro';
 import "fontsource-roboto";
 import { validarCPF, validarSenha } from './models/cadastro';
+import ValidacoesCadastro from './Contexts/ValidaçõesCadastro';
+
 class App extends Component {
   render(){
   return (
     <Container component="article" maxWidth="sm" >
     <Typography variant='h3' align='center'component='h1'>Formulário de Cadastro</Typography>
-    <FormularioCadastro aoEnviar={aoEnviarForm} validacoes={{cpf:validarCPF, senha:validarSenha, nome:validarSenha}}/>
+    <ValidacoesCadastro.Provider 
+    value={{cpf:validarCPF, senha:validarSenha, nome:validarSenha}}>
+    <FormularioCadastro aoEnviar={aoEnviarForm}/>
+    </ValidacoesCadastro.Provider>
     </Container>
   );
  }
